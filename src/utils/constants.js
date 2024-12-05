c05c31a75bd034001af9e2bf569df600bb3a6b80
Amardeep001 <myamardeepsingh001@gmail.com>
2024-12-05 06:31:08 +0000

    dot https url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-export const API_URL = "http://20.244.107.209:8005/";
+export const API_URL = "http://20.244.107.209:8005/dot/";
+// http://20.244.107.209:8005/
 // http://20.244.107.209:8004/
 // https://github.com/Amardeep001/dot-bot
